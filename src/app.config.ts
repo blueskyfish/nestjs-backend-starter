64b9d6cd837b6dd51b9410f8d1babd587285eb02
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-04-18 11:40:33 +0200

    Startup Code
    Add the reading from environment
    Add global filter for exception
    Add Global Validation
    Config and update OpenAPI
    Prepare documentation in Readme file

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,29 @@
 
+/**
+ * The default host of the backend server
+ */
+export const DEFAULT_HOST = 'localhost';
+
+/**
+ * The default database host name
+ */
 export const DEFAULT_DB_HOST = 'localhost';
+
+/**
+ * The default database port
+ */
 export const DEFAULT_DB_PORT = 3306;
 
 /**
  * The list of environment name
  */
 export enum EnvName {
+
+  /** Environment variable for the backend server host */
+  Host = 'HOST',
+  /** Environment variable for the backend server port */
+  PORT = 'PORT',
+
   DbHost = 'DB_HOST',
   DbPort = 'DB_PORT',
   DbUser = 'DB_USER',
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiProduces, ApiProperty } from '@nestjs/swagger';
+import { MaxLength, ValidateIf } from 'class-validator';
 import { AppService } from './app.service';
+import { ErrorBody } from './common/error';
+
+// TODO: One Class per file: Mode to own file
+export class QueryHelloParams {
+
+  @ApiProperty({description: 'Your Name (max 20 signs)', required: false})
+  @ValidateIf((p) => !!p.name)
+  @MaxLength(20, {message: 'The name is too long (max 20 signs)'})
+  readonly name: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+    description: 'Get hello world',
+    operationId: 'getHello'
+  })
+  @ApiProduces('text/plain')
+  @ApiOkResponse({
+    status: 200,
+    description: 'The Hello response',
+    type: String
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'The validation error message',
+    type: ErrorBody
+  })
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query() params: QueryHelloParams): string {
+    return this.appService.getHello(params.name);
   }
 }
