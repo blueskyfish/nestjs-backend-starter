35f5666bd34bbaa2bcf93449d068a40dc4f350dc
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-04-28 12:08:03 +0200

    User Service: create user table

---
+++ docker/db/sql/01-create-tables.sql
@@ -0,0 +1,27 @@
+--
+-- Create the tables into the database "starter"
+--
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+USE `starter`;
+
+--
+-- User table (user_id begins with 1000)
+--
+CREATE TABLE `starter`.`starter_users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT COMMENT 'The unique user id' ,
+  `name` VARCHAR(60) NOT NULL COMMENT 'The user name' ,
+    `password` varchar(250) NOT NULL COMMENT 'The user password',
+  `email` VARCHAR(250) NOT NULL COMMENT 'The user email' ,
+  `roles` VARCHAR(250) NOT NULL COMMENT 'The json array with the role names' ,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UNI_EMAIL` (`email`)
+) ENGINE = InnoDB COMMENT = '(Nestjs Starter)' AUTO_INCREMENT=1000;
+
+
+COMMIT;
