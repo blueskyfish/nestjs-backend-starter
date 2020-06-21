46d0d3abedc5c4a7e4ac882a9d33f4be0eaff466
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-06-21 16:40:59 +0200

    improve auth middleware
    delete unused classes
    add jsdoc

--- src/business/middleware/auth-middleware.util.ts
+++ src/business/middleware/auth-middleware.util.ts
@@ -1,16 +1,31 @@
 import { Request } from 'express';
-import { AuthUser } from '../../auth/auth.user';
+import { AuthUser } from '../../auth';
 
+/**
+ * The dummy user
+ */
 const DUMMY = new AuthUser({
   id: -1,
   device: -1,
   roles: [],
 });
 
+/**
+ * Update the request with the auth user.
+ *
+ * @param {e.Request} req the request
+ * @param {AuthUser} authUser the auth user
+ */
 export function updateRequest(req: Request, authUser: AuthUser) {
   (req as any).authUser = authUser;
 }
 
+/**
+ * Get the auth user from the request and if the request has no user, then it returns the dummy user.
+ *
+ * @param {e.Request} req the request
+ * @returns {AuthUser} the auth user
+ */
 export function getAuthUser(req: Request): AuthUser {
   return (req as any).authUser || DUMMY;
 }
