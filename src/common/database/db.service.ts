ea26c71e87fb5feaa14c1585482c4365557d5228
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-06-21 16:28:29 +0200

    remove console messages and add logger from nestjs

--- src/common/database/db.service.ts
+++ src/common/database/db.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationShutdown } from '@nestjs/common';
 import { Moment } from 'moment';
 import { createPool, MysqlError, Pool, PoolConnection } from 'mysql';
 import { DateUtil, forEachIterator } from '../util';
@@ -28,7 +28,7 @@ export class DbService implements OnApplicationShutdown {
 
   private readonly _pool: Pool;
 
-  constructor(private config: DbConfig) {
+  constructor(private logger: Logger, private config: DbConfig) {
     // create the pool
     this._pool = createPool({
       host: config.host,
@@ -68,7 +68,7 @@ export class DbService implements OnApplicationShutdown {
 
     // register pool events
     this._pool.on('acquire', (conn: PoolConnection) => {
-      // console.info('> Info: Acquire connection [%s]', conn.threadId);
+      this.logger.debug(`Acquire connection [${conn.threadId}]`);
       if (conn.threadId) {
         this.durationMap.set(conn.threadId, DateUtil.now());
       }
@@ -78,25 +78,25 @@ export class DbService implements OnApplicationShutdown {
     this._pool.on('enqueue', (err: MysqlError) => this.handleEnqueueError(err));
 
     this._pool.on('connection', (conn: PoolConnection) => {
-      console.info('> Info: connection [%s] requested', conn.threadId);
+      this.logger.debug(`connection [${conn.threadId}] requested`);
     });
 
     this._pool.on('release', (conn: PoolConnection) => {
       if (conn.threadId) {
         const startTime = this.durationMap.get(conn.threadId);
         if (!startTime) {
-          console.warn('> Warn: The connection [%s] has no start time', conn.threadId);
+          this.logger.warn(`> Warn: The connection [${conn.threadId}] has no start time`);
           return;
         }
 
         const endTime = DateUtil.now();
 
         const milliSeconds = endTime.diff(startTime, 'milliseconds');
-        console.info('> Info: connection [%s] is monitored with %s milliseconds', conn.threadId, milliSeconds);
+        this.logger.debug(`connection [${conn.threadId}] is monitored with ${milliSeconds} milliseconds`);
 
         this.durationMap.delete(conn.threadId);
       } else {
-        console.warn('> Warn: Connection could not be monitored');
+        this.logger.warn('> Warn: Connection could not be monitored');
       }
     });
   }
@@ -107,25 +107,25 @@ export class DbService implements OnApplicationShutdown {
    * @returns {DbConnection}
    */
   getConnection(): DbConnection {
-    return new DbConnection(this._pool);
+    return new DbConnection(this.logger, this._pool);
   }
 
   private async shutdown(): Promise<void> {
     return new Promise(((resolve, reject) => {
       if (this._pool) {
 
-        console.info('> Info: start to close database pool...');
+        this.logger.log('start to close database pool...');
 
         // show error counters
         forEachIterator(this.errorCounter.entries(), ([ code, count ]) => {
-          console.info('> Info:  %s => %s', count, code);
+          this.logger.log(`Error Counter:  ${count} => ${code}`);
         });
         this.errorCounter.clear();
 
         if (this.durationMap.size > 0) {
 
           forEachIterator(this.durationMap.entries(), ([ threadId, startTime ]) => {
-            console.info('> Duration [%s] => %s', threadId, DateUtil.formatTimestamp(startTime));
+            this.logger.log(`Duration [${threadId}] => ${DateUtil.formatTimestamp(startTime)}`);
           });
 
           this.durationMap.clear();
@@ -133,12 +133,12 @@ export class DbService implements OnApplicationShutdown {
 
         this._pool.end((err: MysqlError) => {
           if (err) {
-            console.error('> Error: Query Error: %s -> %s', err.code, err.message);
-            console.error('> Error: Stack: \n%s', err.stack);
+            this.logger.error(`> Error: Query Error: ${err.code} -> ${err.message}`);
+            this.logger.error(`> Error: Stack: \n${err.stack}`);
             return reject(err);
           }
 
-          console.info('> Info: finish of close database pool...');
+          this.logger.log('> Info: finish of close database pool...');
           resolve();
         });
       }
@@ -154,7 +154,7 @@ export class DbService implements OnApplicationShutdown {
       const code = DbUtil.adjustAndLower(err.code, '.');
       const message = err.sqlMessage;
       const sql = err.sql;
-      console.error('> Error: %s => %s\n%s\n-----', code, message, sql);
+      this.logger.error(`> Error: ${code} => ${message}\n${sql}\n-----`);
 
       const count = this.errorCounter.get(code) || 0;
       this.errorCounter.set(code, count + 1);
