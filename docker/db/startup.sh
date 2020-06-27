5775743c40ee559314f8b46af18bd5e101f551be
Blueskyfish <blueskyfish@blueskyfish.de>
2020-06-27 19:04:17 +0200

    Improve shell script "start.sh"
    and improve the log message

--- docker/db/startup.sh
+++ docker/db/startup.sh
@@ -3,15 +3,15 @@
 # TODO Rename "Starter" to your Short Project Name
 
 if [ -f /app/data/createDB ]; then
-  echo "> [info] (Starter) MySQL directory already present, skipping creation"
+  echo ">>>>>> (Starter) MySQL directory already present, skipping creation"
 else
-  echo "> [info] (Starter) MySQL data directory not found, creating initial DBs"
+  echo ">>>>>> (Starter) MySQL data directory not found, creating initial DBs"
 
   mysql_install_db --user=root > /dev/null
 
   if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
     MYSQL_ROOT_PASSWORD=111111
-    echo "> [info] (Starter) MySQL root Password: $MYSQL_ROOT_PASSWORD"
+    echo ">>>>>> (Starter) MySQL root Password: $MYSQL_ROOT_PASSWORD"
   fi
 
   MYSQL_DATABASE=${MYSQL_DATABASE:-""}
@@ -36,11 +36,11 @@ UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
 EOF
 
   if [ "$MYSQL_DATABASE" != "" ]; then
-    echo "> [info] (Starter) Creating database: $MYSQL_DATABASE"
+    echo ">>>>>> (Starter) Creating database: $MYSQL_DATABASE"
     echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
     if [ "$MYSQL_USER" != "" ]; then
-      echo "> [info] (Starter) Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+      echo ">>>>>> (Starter) Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
       echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
     fi
 
@@ -57,14 +57,14 @@ EOF
   cat `ls -- /sql/*.sql | sort` > /sql/all.sql
   /usr/bin/mysqld --user=root --bootstrap --verbose=0 < /sql/all.sql
 
-  echo "> [info] (Starter) Test database"
+  echo ">>>>>> (Starter) Test database"
   sed 's/`starter`/`starter-test`/g' /sql/all.sql > /sql/all-test.sql
   /usr/bin/mysqld --user=root --bootstrap --verbose=0 < /sql/all-test.sql
 
-  echo "> [info] (Starter) Finish Test database"
+  echo ">>>>>> (Starter) Finish Test database"
 
   if [ ! -f /app/data/createDB ]; then
-    echo "> [info] (Starter) create file /app/data/createDB"
+    echo ">>>>>> (Starter) create file /app/data/createDB"
     touch /app/data/createDB
   fi
 
