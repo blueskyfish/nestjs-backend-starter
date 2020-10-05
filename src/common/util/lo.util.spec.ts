92dad19342c633611a9df4653bef62c6469e8cae
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-10-05 21:15:25 +0200

    Replace lodash
    add tests

---
+++ src/common/util/lo.util.spec.ts
@@ -0,0 +1,181 @@
+import { LoUtil } from './lo.util';
+
+describe('LoUtil', () => {
+
+  describe('isNil', () => {
+
+    it('should return "true"', () => {
+      const dd = null;
+      const rec: any = {};
+      expect(LoUtil.isNil(dd)).toBeTruthy();
+      expect(LoUtil.isNil(rec.date)).toBeTruthy();
+    });
+
+    it('should return "false"', () => {
+      expect(LoUtil.isNil(1)).toBeFalsy();
+      expect(LoUtil.isNil(true)).toBeFalsy();
+      expect(LoUtil.isNil('test')).toBeFalsy();
+    });
+
+    it('"NaN" should return "false"', () => {
+      expect(LoUtil.isNil(NaN)).toBeFalsy();
+    });
+
+  });
+
+  describe('isString', () => {
+
+    it('empty string should return "true"', () => {
+      expect(LoUtil.isString('')).toBeTruthy();
+    });
+
+    it('should be a string', () => {
+      const text = 'ABC';
+      expect(LoUtil.isString(text)).toBeTruthy();
+      expect(LoUtil.isString(3.14567.toFixed(2))).toBeTruthy();
+    });
+
+    it('should not be a string', () => {
+      let ss;
+      expect(LoUtil.isString(null)).toBeFalsy();
+      expect(LoUtil.isString(ss)).toBeFalsy();
+      expect(LoUtil.isString(1)).toBeFalsy();
+      expect(LoUtil.isString(new Date())).toBeFalsy();
+      expect(LoUtil.isString(NaN)).toBeFalsy();
+    });
+  });
+
+  describe('isFunction', () => {
+
+    it('should not a function', () => {
+      expect(LoUtil.isFunction(1)).toBeFalsy();
+      expect(LoUtil.isFunction('today')).toBeFalsy();
+      expect(LoUtil.isFunction(null)).toBeFalsy();
+      expect(LoUtil.isFunction(undefined)).toBeFalsy();
+    });
+
+    it('should a function', () => {
+      function hello(hello: string) {
+        console.log(hello);
+      }
+      const calc = (x: number, y: number): number => x +y;
+
+      expect(LoUtil.isFunction(hello)).toBeTruthy();
+      expect(LoUtil.isFunction(calc)).toBeTruthy();
+    });
+  });
+
+  describe('toLower', () => {
+
+    it('should convert to lowercase', () => {
+      expect(LoUtil.toLower('Abc')).toEqual('abc');
+      expect(LoUtil.toLower('abc')).toEqual('abc');
+      expect(LoUtil.toLower('ABC')).toEqual('abc');
+    });
+
+    it('empty string is null', () => {
+      expect(LoUtil.toLower('')).toBeNull();
+    });
+
+    it('Null or undefined should return null', () => {
+      expect(LoUtil.toLower(null)).toBeNull();
+      expect(LoUtil.toLower(undefined)).toBeNull();
+    });
+
+  });
+
+
+  describe('toUpper', () => {
+
+    it('should convert to lowercase', () => {
+      expect(LoUtil.toUpper('Abc')).toEqual('ABC');
+      expect(LoUtil.toUpper('abc')).toEqual('ABC');
+      expect(LoUtil.toUpper('ABC')).toEqual('ABC');
+    });
+
+    it('empty string is null', () => {
+      expect(LoUtil.toUpper('')).toBeNull();
+    });
+
+    it('Null or undefined should return null', () => {
+      expect(LoUtil.toUpper(null)).toBeNull();
+      expect(LoUtil.toUpper(undefined)).toBeNull();
+    });
+
+  });
+
+  describe('trim', () => {
+
+    it('should trim string', () => {
+      expect(LoUtil.trim('  ABC  ')).toEqual('ABC');
+    });
+
+    it ('Empty string return null', () => {
+      expect(LoUtil.trim('')).toBeNull();
+    });
+
+    it('Null or undefined return null', () => {
+      expect(LoUtil.trim(null)).toBeNull();
+      expect(LoUtil.trim(undefined)).toBeNull();
+    });
+  });
+
+  describe('startsWith', () => {
+
+    it('should return true', () => {
+      expect(LoUtil.startsWith('test1234', 'test')).toBeTruthy();
+      expect(LoUtil.startsWith('test1234', 'est', 1)).toBeTruthy();
+    });
+
+    it('should return false', () => {
+      expect(LoUtil.startsWith(null, 'test')).toBeFalsy();
+      expect(LoUtil.startsWith(null, null)).toBeFalsy();
+      expect(LoUtil.startsWith('test1234', null)).toBeFalsy();
+    });
+
+  });
+
+  describe('endsWith', () => {
+
+    it('should return true', () => {
+      expect(LoUtil.endsWidth('test1234', '1234')).toBeTruthy();
+      expect(LoUtil.endsWidth('test1234', '123', 'test1234'.length - 1)).toBeTruthy();
+    });
+
+    it('should return false', () => {
+      expect(LoUtil.endsWidth(null, 'test')).toBeFalsy();
+      expect(LoUtil.endsWidth(null, null)).toBeFalsy();
+      expect(LoUtil.endsWidth('test1234', null)).toBeFalsy();
+    });
+
+  });
+
+  describe('get', () => {
+    const d = {
+      list: [
+        'ab', 'cd'
+      ],
+      flag: true,
+      some: {
+        age: 12,
+        name: 'Sam'
+      },
+    };
+
+    it('get from list', () => {
+      expect(LoUtil.get(d, 'list[0]', null)).toEqual('ab');
+      expect(LoUtil.get(d, 'list[1]', null)).toEqual('cd');
+      expect(LoUtil.get(d, 'list[99]', null)).toBeNull();
+    });
+
+    it('get value', () => {
+      expect(LoUtil.get(d, 'flag', false)).toEqual(true);
+    });
+
+    it('get sub value', () => {
+      expect(LoUtil.get(d, 'some.name', null)).toEqual('Sam');
+      expect(LoUtil.get(d, 'some.age', NaN)).toEqual(12);
+    });
+  })
+
+});
--- src/common/util/lo.util.ts
+++ src/common/util/lo.util.ts
@@ -1,4 +1,13 @@
 
+/**
+ * Small helper class for replace [lodash](https://lodash.com/)
+ *
+ * **Reason**
+ *
+ * * TreeShacking
+ *
+ * @see <https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore>
+ */
 export class LoUtil {
 
   static isNil(v: any): boolean {
@@ -6,7 +15,7 @@ export class LoUtil {
   }
 
   static isString(v: any): boolean {
-    return v && typeof v.valueOf() === 'string';
+    return !LoUtil.isNil(v) && typeof v.valueOf() === 'string';
   }
 
   static isDate(d: any): boolean {
@@ -23,6 +32,34 @@ export class LoUtil {
 
   static toUpper(s: string): string {
     return s ? s.toUpperCase() : null;
+  }
+
+  static trim(s: string): string {
+    return s ? s.trim() : null;
+  }
+
+  static startsWith(s: string, search: string, offset?: number): boolean {
+    if (LoUtil.isString(s) && LoUtil.isString(search)) {
+      return typeof offset === 'number' ? s.startsWith(search, offset) : s.startsWith(search);
+    }
+    return false;
+  }
+
+  static endsWidth(s: string, search: string, offset?: number): boolean {
+    if (LoUtil.isString(s) && LoUtil.isString(search)) {
+      return typeof offset === 'number' ? s.endsWith(search, offset) : s.endsWith(search);
+    }
+    return false;
+  }
+
+  static get<T>(obj: object, path: string, defaultValue?: T): T {
+    const travel = regexp =>
+      String.prototype.split
+        .call(path, regexp)
+        .filter(Boolean)
+        .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
+    const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
+    return result === undefined || result === obj ? defaultValue : result;
   }
 
   static isArray(s: any): boolean {
